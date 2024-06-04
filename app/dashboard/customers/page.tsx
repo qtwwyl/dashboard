3f5f0951754e36694f04cde943e0bafa57d42bea
yl <qtwwyl@163.com>
2024-06-04 17:35:07 +0800

    page add display name

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,4 @@
-export default () => {
+export default function CustomerPage() {
     return (
         <div>
             customers page
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,4 @@
-export default () => {
+export default function InvoicePage() {
     return (
         <div>
             invoices page
